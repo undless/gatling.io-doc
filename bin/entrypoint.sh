4964247da52f8121a4329c9a6e1f94d80dfc06ab
Guillaume Corré <gui.corre@gmail.com>
2022-02-04 15:53:50 +0100

    Gatling OSS 3.7 now have its own branch

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -192,7 +192,7 @@ prepare () {
     hugo_structure_version  "frontline-doc"        "1.15"    "content"           "enterprise/self-hosted"  "1.15"
     hugo_structure_version  "frontline-doc"        "1.14"    "content"           "enterprise/self-hosted"  "1.14"
     hugo_structure_version  "frontline-doc"        "1.13"    "content"           "enterprise/self-hosted"  "1.13"
-    hugo_structure_version  "gatling"              "main"    "src/docs/content"  "gatling"                 "3.7"      true
+    hugo_structure_version  "gatling"              "3.7"     "src/docs/content"  "gatling"                 "3.7"      true
     hugo_structure_version  "gatling"              "3.6"     "src/docs/content"  "gatling"                 "3.6"
     hugo_structure_version  "gatling"              "3.5"     "src/docs/content"  "gatling"                 "3.5"
     hugo_structure_version  "gatling"              "3.4"     "src/docs/content"  "gatling"                 "3.4"
