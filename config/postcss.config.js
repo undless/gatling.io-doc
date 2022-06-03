bec9f9a781afc14fcd0cfdbcc0502a0d3504285c
Guillaume Corré <gcorre@gatling.io>
2022-06-03 16:11:16 +0200

    chore: add missing postcss config

---
+++ config/postcss.config.js
@@ -0,0 +1,7 @@
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const whitelister = require("purgecss-whitelister");
+
+module.exports = {
+  plugins: [autoprefixer()],
+};
